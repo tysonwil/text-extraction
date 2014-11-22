611092df8252066c14d5b422245109e6e529d7ea
Tyson Williams <tyson@dyn-72-33-8-146.uwnet.wisc.edu>
2014-11-22 17:57:38 -0600

    Initial Commit

---
+++ TextExtractor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextExtractor
+//
+//  Created by Tyson Williams on 11/22/14.
+//  Copyright (c) 2014 Tyson Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextExtractor/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TextExtractor
+//
+//  Created by Tyson Williams on 11/22/14.
+//  Copyright (c) 2014 Tyson Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
